d95def45f37480468dae17c4f99827a72be848e7
Harsh Nayak <harshnyk123@gmail.com>
2026-04-20 13:51:42 +0530

    mongodb connection established

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-const app = express();
-const port = 3000;
+dotenv.config({path: "./.env"});
+    
+connectDB();
 
-app.get("/",(req,res)=>{
-    res.send("hello world!")
-})
-
-app.listen(port,()=> {
-    console.log(`Server is running on port ${port}`);
-})
