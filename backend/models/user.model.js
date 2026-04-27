e856205283377307575c96d9866d4a2df0325f46
Harsh Nayak <harshnyk123@gmail.com>
2026-04-27 17:01:14 +0530

    added auth middleware

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,6 +1,12 @@
 import mongoose,{Schema} from 'mongoose';
 
 const userSchema=new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
     name:{
         type: String,
         required: true
@@ -13,7 +19,55 @@ const userSchema=new Schema({
     upiPin:{
         type:String,
         required:true
+    },
+    avatar:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:[true,"Please enter a password"]
     }
 },{timestamps:true});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAcessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            name: this.name
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User=mongoose.model("User",userSchema)
