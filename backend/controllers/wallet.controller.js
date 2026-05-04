af8ddfd38b7a954477b1c9fe06e9908fd7f0a815
Harsh Nayak <harshnyk123@gmail.com>
2026-05-04 14:58:27 +0530

    starting frontend from here

--- backend/controllers/wallet.controller.js
+++ backend/controllers/wallet.controller.js
@@ -25,29 +25,11 @@ const createWallet = asyncHandler(async(req, res)=>{
     ))
 
 })
-const addMoneyToWallet = asyncHandler(async(req, res)=>{
-    const { amount } = req.body
 
-    if(!amount || amount <= 0){
-        throw new ApiError(400, "Invalid amount")
-    }
+const addMoneyToWallet = asyncHandler(async (req, res) => {
 
-    const wallet = await Wallet.findOne({ userId: req.user._id })
-    if(!wallet){
-        throw new ApiError(404, "Wallet not found")
-    }
+    throw new ApiError(400, "Direct wallet top-up not allowed. Add money via bank account");
 
-    wallet.balance += amount;
-    await wallet.save({validateBeforeSave: false});
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(
-            200,
-            wallet,
-            "Money added to wallet successfully"
-        )
-    )
 })
 
 const getWallet = asyncHandler(async(req, res)=>{
